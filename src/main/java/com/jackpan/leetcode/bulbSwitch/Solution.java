b28b6b937a633f7529eb74a45cc3197b9f932edd
jackpan <826647196@qq.com>
2021-11-16 22:19:17 +0800

    Initial commit

---
+++ src/main/java/com/jackpan/leetcode/bulbSwitch/Solution.java
@@ -0,0 +1,12 @@
+package com.jackpan.leetcode.bulbSwitch;
+
+/**
+ * @author jackpan
+ * @version v1.0 2021/11/15 15:15
+ */
+class Solution {
+    public int bulbSwitch(int n) {
+
+        return 1;
+    }
+}
