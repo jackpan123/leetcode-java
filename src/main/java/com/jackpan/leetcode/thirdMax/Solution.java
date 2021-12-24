4f1cb7f84e98f63ec1b0c5affab7358cd89e086a
jackpan <826647196@qq.com>
2021-12-24 23:56:46 +0800

    Update Solution.java

--- src/main/java/com/jackpan/leetcode/thirdMax/Solution.java
+++ src/main/java/com/jackpan/leetcode/thirdMax/Solution.java
@@ -5,21 +5,58 @@ import java.util.Arrays;
 public class Solution {
 
     public int thirdMax(int[] nums) {
-        Arrays.sort(nums);
-        int res = nums[nums.length - 1];
-        int random = 1;
-        if (nums.length >= 3) {
-            for (int i = nums.length - 2; i >= 0; i--) {
-                if (random == 3) {
-                    break;
-                } else {
-                    if (res != nums[i]) {
-                        random++;
-                        res = nums[i];
-                    }
+        int max = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+
+        boolean find = false;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+                find = true;
+            }
+        }
+
+
+        if (find) {
+            find = false;
+            for (int num : nums) {
+                if (num > max2 && num < max) {
+                    max2 = num;
+                    find = true;
                 }
             }
         }
-        return random < 3 ? nums[nums.length - 1] : res;
+
+        if (find) {
+            find = false;
+            for (int num : nums) {
+                if (num >= max3 && num < max2) {
+                    max3 = num;
+                    find = true;
+                }
+            }
+        }
+        return find ? max3 : max;
+
     }
+
+//    public int thirdMax(int[] nums) {
+//        Arrays.sort(nums);
+//        int res = nums[nums.length - 1];
+//        int random = 1;
+//        if (nums.length >= 3) {
+//            for (int i = nums.length - 2; i >= 0; i--) {
+//                if (random == 3) {
+//                    break;
+//                } else {
+//                    if (res != nums[i]) {
+//                        random++;
+//                        res = nums[i];
+//                    }
+//                }
+//            }
+//        }
+//        return random < 3 ? nums[nums.length - 1] : res;
+//    }
 }
