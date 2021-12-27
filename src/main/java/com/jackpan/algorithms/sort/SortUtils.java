eebfe38c0308faae36c94fe988e725c718a1a00e
jackpan <826647196@qq.com>
2021-12-27 11:19:49 +0800

    Add insertion sort

--- src/main/java/com/jackpan/algorithms/sort/SortUtils.java
+++ src/main/java/com/jackpan/algorithms/sort/SortUtils.java
@@ -16,21 +16,42 @@ public class SortUtils {
         }
     }
 
+    public static void insertionSort(int[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            for (int i1 = i; i1 > 0 && numbers[i1] < numbers[i1 - 1]; i1--) {
+                if (numbers[i] > numbers[i1]) {
+                    int temp = numbers[i1];
+                    numbers[i1] = numbers[i1 - 1];
+                    numbers[i1] = temp;
+                }
+            }
+        }
+    }
+
     public static void printArray(int[] numbers) {
         for (int number : numbers) {
             System.out.print(number);
             System.out.print(", ");
         }
+        System.out.println();
     }
 
-    public static void selectSortTest() {
-        int[] numbers = new int[]{0,8,9,7,4,6,65,43};
+
+    public static void selectSortTest(int[] numbers) {
         selectSort(numbers);
+        printArray(numbers);
+
+    }
+
+    public static void insertionSortTest(int[] numbers) {
+        insertionSort(numbers);
         printArray(numbers);
     }
 
     public static void main(String[] args) {
-        selectSortTest();
+        int[] numbers = new int[]{0,8,9,7,4,6,65,43};
+        selectSortTest(numbers);
+        insertionSortTest(numbers);
     }
 
 
