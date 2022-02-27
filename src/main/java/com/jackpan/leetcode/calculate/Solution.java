9e3ebf80908f1626e3bdf477ca7eb9e02a51527d
jackpan <826647196@qq.com>
2022-02-27 23:04:59 +0800

    2022-02-27每日打卡第25题 目标（500题）

--- src/main/java/com/jackpan/leetcode/calculate/Solution.java
+++ src/main/java/com/jackpan/leetcode/calculate/Solution.java
@@ -10,87 +10,87 @@ import java.util.Map;
  * 2022-02-27每日打卡第25题 目标（500题）
  */
 public class Solution {
-    // 使用 map 维护一个运算符优先级
-    // 这里的优先级划分按照「数学」进行划分即可
+
     Map<Character, Integer> map = new HashMap<Character, Integer>(){{
         put('-', 1);
         put('+', 1);
         put('*', 2);
         put('/', 2);
-        put('%', 2);
-        put('^', 3);
     }};
     public int calculate(String s) {
-        // 将所有的空格去掉
         s = s.replaceAll(" ", "");
-        char[] cs = s.toCharArray();
-        int n = s.length();
-        // 存放所有的数字
         Deque<Integer> nums = new ArrayDeque<>();
-        // 为了防止第一个数为负数，先往 nums 加个 0
-        nums.addLast(0);
-        // 存放所有「非数字以外」的操作
-        Deque<Character> ops = new ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
-            char c = cs[i];
-            if (c == '(') {
-                ops.addLast(c);
-            } else if (c == ')') {
-                // 计算到最近一个左括号为止
-                while (!ops.isEmpty()) {
-                    if (ops.peekLast() != '(') {
-                        calc(nums, ops);
+        nums.add(0);
+        Deque<Character> operators = new ArrayDeque<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char character = chars[i];
+            if (Character.isDigit(character)) {
+                int j = i;
+                int sum = 0;
+                while (j < chars.length && Character.isDigit(chars[j])) {
+                    System.out.println(chars[j] - '0');
+                    sum = sum * 10 + (chars[j] - '0');
+                    j++;
+                }
+                nums.addLast(sum);
+                i = j - 1;
+            } else {
+
+                while (!operators.isEmpty()) {
+                    char prev = operators.peekLast();
+                    if (map.get(prev) >= map.get(character)) {
+                        calc(nums, operators);
                     } else {
-                        ops.pollLast();
                         break;
                     }
                 }
-            } else {
-                if (isNumber(c)) {
-                    int u = 0;
-                    int j = i;
-                    // 将从 i 位置开始后面的连续数字整体取出，加入 nums
-                    while (j < n && isNumber(cs[j])) u = u * 10 + (cs[j++] - '0');
-                    nums.addLast(u);
-                    i = j - 1;
-                } else {
-                    if (i > 0 && (cs[i - 1] == '(' || cs[i - 1] == '+' || cs[i - 1] == '-')) {
-                        nums.addLast(0);
-                    }
-                    // 有一个新操作要入栈时，先把栈内可以算的都算了
-                    // 只有满足「栈内运算符」比「当前运算符」优先级高/同等，才进行运算
-                    while (!ops.isEmpty() && ops.peekLast() != '(') {
-                        char prev = ops.peekLast();
-                        if (map.get(prev) >= map.get(c)) {
-                            calc(nums, ops);
-                        } else {
-                            break;
-                        }
-                    }
-                    ops.addLast(c);
-                }
+
+                operators.addLast(character);
             }
         }
-        // 将剩余的计算完
-        while (!ops.isEmpty()) calc(nums, ops);
+
+        while (!operators.isEmpty()) {
+            calc(nums, operators);
+        }
+
         return nums.peekLast();
     }
-    void calc(Deque<Integer> nums, Deque<Character> ops) {
-        if (nums.isEmpty() || nums.size() < 2) return;
-        if (ops.isEmpty()) return;
-        int b = nums.pollLast(), a = nums.pollLast();
-        char op = ops.pollLast();
-        int ans = 0;
-        if (op == '+') ans = a + b;
-        else if (op == '-') ans = a - b;
-        else if (op == '*') ans = a * b;
-        else if (op == '/')  ans = a / b;
-        else if (op == '^') ans = (int)Math.pow(a, b);
-        else if (op == '%') ans = a % b;
-        nums.addLast(ans);
+
+    private void calc(Deque<Integer> nums, Deque<Character> operators) {
+        if (nums.isEmpty() || nums.size() < 2) {
+            return;
+        }
+        if (operators.isEmpty()) {
+            return;
+        }
+
+        Integer b = nums.pollLast();
+        Integer a = nums.pollLast();
+        Character pop = operators.pollLast();
+        switch (pop) {
+            case '+':
+                nums.addLast(a + b);
+                break;
+            case '-':
+                nums.addLast(a - b);
+                break;
+            case '*':
+                nums.addLast(a * b);
+                break;
+            default :
+                nums.addLast(a / b);
+                break;
+        }
+
     }
-    boolean isNumber(char c) {
-        return Character.isDigit(c);
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int calculate = solution.calculate("0-2147483647");
+        System.out.println();
     }
+
+
 }
 
