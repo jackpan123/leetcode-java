835ae5d8b695b9930fc7b9cc3736d3af90d4f189
jackpan <826647196@qq.com>
2022-02-27 21:50:36 +0800

    2022-02-27每日打卡第25题 目标（500题）

--- src/main/java/com/jackpan/leetcode/sortList/Solution.java
+++ src/main/java/com/jackpan/leetcode/sortList/Solution.java
@@ -9,39 +9,101 @@ import java.util.List;
  */
 public class Solution {
 
-    public ListNode sortList(ListNode head) {
-        List<ListNode> list = new ArrayList<>();
-        while (head != null) {
-            list.add(head);
-            head = head.next;
-        }
+//    public ListNode sortList(ListNode head) {
+//        List<ListNode> list = new ArrayList<>();
+//        while (head != null) {
+//            list.add(head);
+//            head = head.next;
+//        }
+//
+//        if (list.isEmpty()) {
+//            return head;
+//        }
+//
+//        list.sort((o1, o2) -> {
+//            if (o1.val > o2.val) {
+//                return 1;
+//            } else if (o1.val < o2.val) {
+//                return -1;
+//            }
+//            return 0;
+//        });
+//
+//        ListNode newHead = new ListNode();
+//        ListNode temp = newHead;
+//
+//        for (int i = 0; i < list.size(); i++) {
+//            temp.next = list.get(i);
+//            temp = temp.next;
+//            if (i == list.size() - 1) {
+//                temp.next = null;
+//            }
+//        }
+//
+//        return newHead.next;
+//    }
 
-        if (list.isEmpty()) {
+    public ListNode sortList(ListNode head) {
+        if (head == null) {
             return head;
         }
-
-        list.sort((o1, o2) -> {
-            if (o1.val > o2.val) {
-                return 1;
-            } else if (o1.val < o2.val) {
-                return -1;
-            }
-            return 0;
-        });
-
-        ListNode newHead = new ListNode();
-        ListNode temp = newHead;
-
-        for (int i = 0; i < list.size(); i++) {
-            temp.next = list.get(i);
-            temp = temp.next;
-            if (i == list.size() - 1) {
-                temp.next = null;
+        int length = 0;
+        ListNode node = head;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        ListNode dummyHead = new ListNode(0, head);
+        for (int subLength = 1; subLength < length; subLength <<= 1) {
+            ListNode prev = dummyHead, curr = dummyHead.next;
+            while (curr != null) {
+                ListNode head1 = curr;
+                for (int i = 1; i < subLength && curr.next != null; i++) {
+                    curr = curr.next;
+                }
+                ListNode head2 = curr.next;
+                curr.next = null;
+                curr = head2;
+                for (int i = 1; i < subLength && curr != null && curr.next != null; i++) {
+                    curr = curr.next;
+                }
+                ListNode next = null;
+                if (curr != null) {
+                    next = curr.next;
+                    curr.next = null;
+                }
+                ListNode merged = merge(head1, head2);
+                prev.next = merged;
+                while (prev.next != null) {
+                    prev = prev.next;
+                }
+                curr = next;
             }
         }
-
-        return newHead.next;
+        return dummyHead.next;
     }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+
 
     class ListNode {
         int val;
