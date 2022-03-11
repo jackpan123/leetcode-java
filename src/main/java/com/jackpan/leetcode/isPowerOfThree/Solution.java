3f6ed1896a1bf02d725747ee3f9b7f22646843b1
jackpan <826647196@qq.com>
2022-03-11 13:36:52 +0800

    循环

--- src/main/java/com/jackpan/leetcode/isPowerOfThree/Solution.java
+++ src/main/java/com/jackpan/leetcode/isPowerOfThree/Solution.java
@@ -7,14 +7,23 @@ package com.jackpan.leetcode.isPowerOfThree;
  */
 public class Solution {
 
+//    public boolean isPowerOfThree(int n) {
+//        if (n == 3 || n == 1) {
+//            return true;
+//        }
+//        if (n % 3 != 0 || n == 0) {
+//            return false;
+//        }
+//
+//        return isPowerOfThree(n / 3);
+//    }
+
+
     public boolean isPowerOfThree(int n) {
-        if (n == 3 || n == 1) {
-            return true;
-        }
-        if (n % 3 != 0 || n == 0) {
-            return false;
+        while (n != 0 && n % 3 == 0) {
+            n /= 3;
         }
 
-        return isPowerOfThree(n / 3);
+        return n == 1;
     }
 }
