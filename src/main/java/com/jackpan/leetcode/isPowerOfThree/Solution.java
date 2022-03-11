7515c6be143c2c78cc9c18f927801940946473e7
jackpan <826647196@qq.com>
2022-03-11 13:29:23 +0800

    2022-03-11每日打卡第28题 目标（500题）

---
+++ src/main/java/com/jackpan/leetcode/isPowerOfThree/Solution.java
@@ -0,0 +1,20 @@
+package com.jackpan.leetcode.isPowerOfThree;
+
+
+/**
+ *
+ * 2022-03-11每日打卡第28题 目标（500题）
+ */
+public class Solution {
+
+    public boolean isPowerOfThree(int n) {
+        if (n == 3 || n == 1) {
+            return true;
+        }
+        if (n % 3 != 0 || n == 0) {
+            return false;
+        }
+
+        return isPowerOfThree(n / 3);
+    }
+}
