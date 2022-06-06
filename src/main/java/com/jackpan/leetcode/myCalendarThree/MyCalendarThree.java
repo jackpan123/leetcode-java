c7b4b37213fc7cf921e74df01a939a406476eb38
jackpan <826647196@qq.com>
2022-06-06 23:12:28 +0800

    Update MyCalendarThree.java

--- src/main/java/com/jackpan/leetcode/myCalendarThree/MyCalendarThree.java
+++ src/main/java/com/jackpan/leetcode/myCalendarThree/MyCalendarThree.java
@@ -6,26 +6,31 @@ import java.util.TreeMap;
 /**
  * 每日打卡第31题 目标（500题）
  * MyCalendarThree操作类
+ *
  * @author JackPan
  * @date 2022/06/06 10:34
  **/
 class MyCalendarThree {
-    private TreeMap<Integer, Integer> cnt;
+
+    private Map<Integer, Integer> cnt;
 
     public MyCalendarThree() {
-        cnt = new TreeMap<Integer, Integer>();
+        cnt = new TreeMap<>();
     }
 
     public int book(int start, int end) {
         int ans = 0;
         int maxBook = 0;
+
         cnt.put(start, cnt.getOrDefault(start, 0) + 1);
         cnt.put(end, cnt.getOrDefault(end, 0) - 1);
+
         for (Map.Entry<Integer, Integer> entry : cnt.entrySet()) {
-            int freq = entry.getValue();
-            maxBook += freq;
+
+            maxBook += entry.getValue();
             ans = Math.max(maxBook, ans);
         }
+
         return ans;
     }
 }
