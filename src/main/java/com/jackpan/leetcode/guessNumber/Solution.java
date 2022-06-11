ce9948760df2c8d26e05d8a0ddf6d1557c5ce557
jackpan <826647196@qq.com>
2022-06-11 11:05:14 +0800

    Update Solution.java

--- src/main/java/com/jackpan/leetcode/guessNumber/Solution.java
+++ src/main/java/com/jackpan/leetcode/guessNumber/Solution.java
@@ -7,24 +7,40 @@ package com.jackpan.leetcode.guessNumber;
  * @date 2022/06/11 10:34
  **/
 public class Solution {
+//    public int guessNumber(int n) {
+//        int low = 1;
+//        int high = n;
+//        int ans = n;
+//        while (low <= high) {
+//            int mid = ((high - low) >> 2) + low ;
+//            int result = guess(mid);
+//            if (result == 0) {
+//                ans = mid;
+//                break;
+//            } else if (result > 0) {
+//                low = mid + 1;
+//            } else {
+//                high = mid - 1;
+//            }
+//        }
+//
+//        return ans;
+//    }
+
+    // 优化
     public int guessNumber(int n) {
         int low = 1;
         int high = n;
-        int ans = n;
-        while (low <= high) {
+        while (low < high) {
             int mid = ((high - low) >> 2) + low ;
-            int result = guess(mid);
-            if (result == 0) {
-                ans = mid;
-                break;
-            } else if (result > 0) {
-                low = mid + 1;
+            if (guess(mid) <= 0) {
+                high = mid;
             } else {
-                high = mid - 1;
+                low = mid + 1;
             }
         }
 
-        return ans;
+        return low;
     }
 
     public int guess(int num) {
