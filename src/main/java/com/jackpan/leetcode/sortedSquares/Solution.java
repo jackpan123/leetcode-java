2ef1bcb0f7ea3cf4d7daa49f916aa23351569871
jackpan <826647196@qq.com>
2022-08-10 13:05:28 +0800

    68-977

--- src/main/java/com/jackpan/leetcode/sortedSquares/Solution.java
+++ src/main/java/com/jackpan/leetcode/sortedSquares/Solution.java
@@ -10,15 +10,58 @@ import java.util.Arrays;
  **/
 public class Solution {
 
+//    public int[] sortedSquares(int[] nums) {
+//
+//        int[] ans = new int[nums.length];
+//        for (int i = 0; i < nums.length; i++) {
+//            ans[i] = nums[i] * nums[i];
+//        }
+//
+//        Arrays.sort(ans);
+//
+//        return ans;
+//    }
+
+
+    /**
+     * 双指针
+     * @param nums
+     * @return
+     */
     public int[] sortedSquares(int[] nums) {
 
-        int[] ans = new int[nums.length];
+        int n = nums.length;
+        int negative = -1;
+
         for (int i = 0; i < nums.length; i++) {
-            ans[i] = nums[i] * nums[i];
+            if (nums[i] < 0) {
+                negative = i;
+            } else {
+                break;
+            }
         }
 
-        Arrays.sort(ans);
+        int[] ans = new int[n];
 
+        int i = negative;
+        int j = negative + 1;
+        int index = 0;
+        while (i >=0 || j < n) {
+            if (i < 0) {
+                ans[index] = nums[j] * nums[j];
+                j++;
+            } else if (j >= n) {
+                ans[index] = nums[i] * nums[i];
+                i--;
+            } else if (nums[i] * nums[i] < nums[j] * nums[j]) {
+                ans[index] = nums[i] * nums[i];
+                i--;
+            } else {
+                ans[index] = nums[j] * nums[j];
+                j++;
+            }
+            index++;
+        }
         return ans;
     }
 }
