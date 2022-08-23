86925bd076b73b15b7d4ded6b65eb7917ad37410
jackpan <826647196@qq.com>
2022-08-23 13:35:41 +0800

    86-21

--- src/main/java/com/jackpan/leetcode/mergeTwoLists/Solution.java
+++ src/main/java/com/jackpan/leetcode/mergeTwoLists/Solution.java
@@ -5,53 +5,34 @@ package com.jackpan.leetcode.mergeTwoLists;
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = null;
-        ListNode ans = null;
-        if (l1 != null || l2 != null) {
-            if (l1 == null) {
-                head = l2;
-                ans = l2;
-                l2 = l2.next;
-            } else if (l2 == null) {
-                head = l1;
-                ans = l1;
+
+        ListNode root = new ListNode(0);
+        ListNode head = root;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                head.next = l1;
                 l1 = l1.next;
             } else {
-                if (l1.val <= l2.val) {
-                    head = l1;
-                    ans = l1;
-                    l1 = l1.next;
-                } else {
-                    head = l2;
-                    ans = l2;
-                    l2 = l2.next;
-                }
-            }
-        }
-        while (l1 != null || l2 != null) {
-            if (l1 == null) {
                 head.next = l2;
-                break;
-            } else if (l2 == null) {
-                head.next = l1;
-                break;
-            } else {
-                if (l1.val <= l2.val) {
-                    head.next = l1;
-                    l1 = l1.next;
-                } else {
-                    head.next = l2;
-                    l2 = l2.next;
-                }
-                head = head.next;
+                l2 = l2.next;
             }
 
+            head = head.next;
         }
 
-        return ans;
+        if (l1 != null) {
+            head.next = l1;
+        }
+
+        if (l2 != null) {
+            head.next = l2;
+        }
+
+        return root.next;
+
     }
 
-    class ListNode {
+    static class ListNode {
         int val;
         ListNode next;
 
