f23ddcc4e5aa25b714790070f46c3edfedb163e8
jackpan <826647196@qq.com>
2022-08-23 13:15:46 +0800

    Update Solution.java

--- src/main/java/com/jackpan/leetcode/stringMatching/Solution.java
+++ src/main/java/com/jackpan/leetcode/stringMatching/Solution.java
@@ -1,9 +1,7 @@
 package com.jackpan.leetcode.stringMatching;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Solution操作类
@@ -15,20 +13,17 @@ public class Solution {
     public List<String> stringMatching(String[] words) {
 
         List<String> res = new ArrayList<>();
-        Set<String> stringSet = new HashSet<>();
         int len = words.length;
         for (int i = 0; i < len; i++) {
-            String word = words[i];
             for (int j = 0; j < len; j++) {
-                if (i != j && words[i].length() < words[j].length()) {
-                    if (words[j].contains(words[i]) && !stringSet.contains(words[i])) {
-                        res.add(words[i]);
-                        stringSet.add(words[i]);
-                    }
+                if (i != j && words[j].contains(words[i])) {
+                    res.add(words[i]);
+                    break;
                 }
             }
         }
 
         return res;
     }
+
 }
