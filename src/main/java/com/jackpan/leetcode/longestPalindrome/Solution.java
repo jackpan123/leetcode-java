04e64697406d633ad2ec3916bc69f5ebf9430ba1
jackpan <826647196@qq.com>
2022-08-26 12:44:44 +0800

    Update Solution.java

--- src/main/java/com/jackpan/leetcode/longestPalindrome/Solution.java
+++ src/main/java/com/jackpan/leetcode/longestPalindrome/Solution.java
@@ -12,23 +12,42 @@ import java.util.Map;
 public class Solution {
 
     public int longestPalindrome(String s) {
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        int[] count = new int[128];
         for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            count[s.charAt(i)] ++;
         }
 
         int ans = 0;
-        int maxOdd = 0;
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            Integer value = entry.getValue();
-            if (value % 2 != 0) {
-                maxOdd = value;
-                ans += (value - 1);
-            } else {
-                ans += value;
+        for (int num : count) {
+            ans += num / 2 * 2;
+            if (num % 2 != 0 && ans % 2 != 0) {
+                ans++;
             }
         }
 
-        return maxOdd > 0 ? ans + 1 : ans;
+        return ans;
     }
+
+//    public int longestPalindrome(String s) {
+//        Map<Character, Integer> map = new HashMap<Character, Integer>();
+//        for (int i = 0; i < s.length(); i++) {
+//            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+//        }
+//
+//        int ans = 0;
+//        int maxOdd = 0;
+//        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+//            Integer value = entry.getValue();
+//            if (value % 2 != 0) {
+//                maxOdd = value;
+//                ans += (value - 1);
+//            } else {
+//                ans += value;
+//            }
+//        }
+//
+//        return maxOdd > 0 ? ans + 1 : ans;
+//    }
+
+
 }
