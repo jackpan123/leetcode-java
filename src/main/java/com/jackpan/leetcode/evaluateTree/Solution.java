43e8f280b9d7a744a9ea84f80d2ba5e5a821647c
JackPan <826647196@qq.com>
2023-02-06 22:31:24 +0800

    139-2331

---
+++ src/main/java/com/jackpan/leetcode/evaluateTree/Solution.java
@@ -0,0 +1,39 @@
+package com.jackpan.leetcode.evaluateTree;
+
+/**
+ * Solution操作类
+ *
+ * @author JackPan
+ * @date 2023/02/06 22:30
+ **/
+public class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null) {
+            return root.val == 1;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        } else {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
