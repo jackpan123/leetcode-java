ce561a90fbac8d682f9f275212782c7e7642271f
JackPan <826647196@qq.com>
2023-09-19 10:09:17 +0800

    Update MergeSortedArray.java

--- src/main/java/com/jackpan/leetcode/classic150/MergeSortedArray.java
+++ src/main/java/com/jackpan/leetcode/classic150/MergeSortedArray.java
@@ -9,25 +9,22 @@ package com.jackpan.leetcode.classic150;
 public class MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = nums1.length - 1; i >= 0; i--) {
-            if (m > 0 && n == 0) {
-                nums1[i] = nums1[m - 1];
-                m--;
-                continue;
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int tail = m + n - 1;
+        int curr;
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 == -1) {
+                curr = nums2[p2--];
+            } else if (p2 == -1) {
+                curr = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                curr = nums1[p1--];
+            } else {
+                curr = nums2[p2--];
             }
 
-            if (n > 0 && m == 0) {
-                nums1[i] = nums2[n - 1];
-                n--;
-                continue;
-            }
-            if (nums1[m - 1] > nums2[n - 1]) {
-                nums1[i] = nums1[m - 1];
-                m--;
-            } else {
-                nums1[i] = nums2[n - 1];
-                n--;
-            }
+            nums1[tail--] = curr;
         }
     }
 }
