e06a846dfbf368d9696745eab6bca93a15253817
JackPan <826647196@qq.com>
2023-09-22 14:16:01 +0800

    Update RemoveDuplicates.java

--- src/main/java/com/jackpan/leetcode/classic150/RemoveDuplicates.java
+++ src/main/java/com/jackpan/leetcode/classic150/RemoveDuplicates.java
@@ -10,18 +10,17 @@ public class RemoveDuplicates {
 
     public int removeDuplicates(int[] nums) {
         int len = nums.length;
-        int p1 = 0;
-        int p2 = 0;
-        while (p1 < len && p2 < len) {
-            if (p1 == p2) {
-                p2++;
-            } else if (nums[p1] == nums[p2]) {
-                p2++;
-            } else {
-                nums[++p1] = nums[p2++];
+
+        int fast = 1;
+        int slow = 1;
+        while (fast < len) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                slow++;
             }
+            fast++;
         }
 
-        return p1 + 1;
+        return slow;
     }
 }
