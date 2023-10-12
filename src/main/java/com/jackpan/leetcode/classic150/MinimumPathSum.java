129ac056eb4ee05c789599ad7dd275b320115d1e
JackPan <826647196@qq.com>
2023-10-12 14:39:24 +0800

    Update MinimumPathSum.java

--- src/main/java/com/jackpan/leetcode/classic150/MinimumPathSum.java
+++ src/main/java/com/jackpan/leetcode/classic150/MinimumPathSum.java
@@ -15,22 +15,26 @@ public class MinimumPathSum {
      * @return
      */
     public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
         int rows = grid.length;
         int columns = grid[0].length;
 
         int[][] dp = new int[rows][columns];
-        dp[0][0] = 0;
-        for (int i = 1; i < columns; i++) {
-            dp[0][i] = dp[0][i - 1] + grid[0][i - 1];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
         }
 
-        for (int j = 1; j < rows; j++) {
-            dp[j][0] = dp[j - 1][0] + grid[j - 1][0];
+        for (int j = 1; j < columns; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
         }
 
-        for (int j = 1; j < rows; j++) {
-            for (int i = 1; i < columns; i++) {
-                dp[j][i] = Math.min(dp[j][i - 1], dp[j - 1][i]) + grid[j][i];
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
 
